507f58568818cd28d09840ed22d5f5917fd662c2
ma7moudshoman <shomanm53@gmail.com>
2024-04-03 01:20:58 +0200

    Revision

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+       Scanner input = new Scanner(System.in);
+      //  System.out.println("Inter the grade Student ");
+      //  int grade = 0;
+       /* if (grade >= 90&& grade<=100) {
+            System.out.println('A');
+        } else if (grade >= 80&&grade<=90) {
+            System.out.println("B");
+        } else if (grade >= 70&&grade<=80) {
+            System.out.println('C');
+        } else if (grade>=60&&grade<=70) {
+            System.out.println('D');
+        }
+        else
+            System.out.println("invalid code");
+        //     System.out.printf("Hello and welcome
+*/
+      //  System.out.println("Please inter one of this operation = * or / or %");
+       // char grade=input.next().charAt(0) ;
+
+       /* String r=(grade >= 90&& grade<=100)?"A":(grade >= 80&&grade<=90)?"B"
+                :(grade >= 70&&grade<=80)?"C":(grade>=60&&grade<=70)?"D":
+                ("invalid code");
+
+        System.out.println(r);*/
+/*
+ String k="AAA";
+int x=5;
+        switch (5 ){
+            case 1 :
+                break;
+            case  2:
+                break;
+
+            case 10/3:
+                System.out.println("Case 2 + 1 ");
+                break;
+
+            default:
+                System.out.println("it is not an option");
+        }*/
+/*
+        int  counter =1 ;
+        while (counter++ <=5 ){
+            System.out.println(counter  + " = Hi");
+            // counter++;
+
+        }
+
+        System.out.println("counter = " + counter);*/
+
+        char letter='a';
+boolean flag=true;
+        int d=0;
+        while (flag)
+        {
+            letter=input.next().charAt(0);
+d++;
+if (letter=='k')
+    flag=false;
+        }
+        System.out.println("invalid "+ d);
+
+    }
+    }
