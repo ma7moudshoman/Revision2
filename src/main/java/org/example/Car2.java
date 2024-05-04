a353c7b1ec740196d601755752bec2fa3b29c47e
ma7moudshoman <shomanm53@gmail.com>
2024-05-04 22:51:29 +0300

    INTERFACE

--- src/main/java/org/example/Car2.java
+++ src/main/java/org/example/Car2.java
@@ -1,15 +1,10 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Car2 {
 
     static void Interface(SelfDrivable selfDrivable){
         selfDrivable.drive();
     }
-
-
     public static void main(String[]args){
       /*  Interface (new CarV1());
         Interface(new CarV2());
@@ -19,8 +14,29 @@ SelfDrivable s=new CarV1();
  s.drive();
       s=new CarV2();
       s.drive();*/
-        GenericInterface <car1> genericInterface=new car1();
+      RewardClass d=new RewardClass();
+        RewardClass d1=new RewardClass(){ @Override
+        public void Class(){
+            System.out.println("your reword is 30 $");
+        }};
+        RewardClass d3=new RewardClass(){
+            @Override
+            public void Class(){
+                System.out.println("your reword is 30 $");
+            }
+        } ;
+        new RewardClass(){
+            @Override
+            public void Class(){
+                System.out.println("your reword is 30 $");
+            }
+        } .Class();
+        System.out.println(d.getClass());
+        System.out.println(d3.getClass());
+        System.out.println(d1.getClass() );
+        d.Class();
+d3.Class();
+        d1.Class();
 
-        car1 car1=genericInterface.carModel();
        }
 }
---
+++ src/main/java/org/example/RewardClass.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public  class RewardClass {
+
+    public  void Class(){
+        System.out.println("your reword is 10 $");
+    }
+
+}
