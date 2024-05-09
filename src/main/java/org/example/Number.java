9fe65fbf075027b8cda48cb09237e2ddd80ac619
ma7moudshoman <shomanm53@gmail.com>
2024-05-09 13:53:17 +0300

    ENUM

---
+++ src/main/java/org/example/Number.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import static org.example.Numbers.THREE;
+
+public class Number {
+    interface test{
+
+    }
+
+    enum Weekday {
+        monday("working day"){
+            @Override
+            public void testmethod(){
+                System.out.println("k;fv;cm");
+            };
+        }, tuesday {
+                    @Override
+                    public void testmethod(){
+                        System.out.println("ghitpt");
+                    }
+                }; //wednesday, thursday, friday(" day Off"), saturday, sunday;
+        String status;
+
+        Weekday(String status) {
+            this.status = status;
+        }
+        public abstract void testmethod();
+       Weekday(){
+
+          }
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+    }
+
+    public static void main(String[]args){
+       Weekday.monday.testmethod();
+      //  System.out.println(Weekday.monday.status);
+
+   /*  final    int One=1;
+      final   int TWO=2;
+        final int THREE=3;*/
+     Numbers val=Numbers.ONE;
+      //  int val=1;
+        switch (val){
+            case ONE :
+                System.out.println("1");
+                break;
+            case TWO:
+                System.out.println("2");
+                break;
+            case THREE:
+                System.out.println("3");
+
+        }
+
+
+    }
+}
---
+++ src/main/java/org/example/Numbers.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public enum Numbers {
+   ONE,TWO,THREE ;
+
+
+
+}
